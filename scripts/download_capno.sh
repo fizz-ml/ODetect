d76d7b2d21bd5c37ba45ae9fe62a6df5bb9c9466
Cheng Xie <cheng.k.xie@gmail.com>
2017-11-05 20:36:06 -0800

    Updating capno download script

--- scripts/download_capno.sh
+++ scripts/download_capno.sh
@@ -1,5 +1,9 @@
 wget "http://www.capnobase.org/uploads/media/TBME2013-PPGRR-Benchmark_R3.zip"
-mv TBME2013-PPGRR-Benchmark_R3.zip ./data/
-cd ./data
+mkdir data
+mkdir data/raw
+mv TBME2013-PPGRR-Benchmark_R3.zip ./data/raw
+cd ./data/raw
 unzip TBME2013-PPGRR-Benchmark_R3.zip
-cd ..
+mv TBME2013-PPGRR-Benchmark_R3 capno
+rm TBME2013-PPGRR-Benchmark_R3.zip
+cd ../..
