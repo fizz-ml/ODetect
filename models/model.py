1e66e093bfd64e326d5f23c922d0de4fe270502b
Curtis Huebner <mathcurt@gmail.com>
2017-11-13 19:19:23 -0800

    added sfft model

---
+++ models/model.py
@@ -0,0 +1,9 @@
+class Model(object):
+    def __call__(x):
+        """Return the next predicted point"""
+        raise NotImplementedError
+        return y
+
+    def reset(x):
+        """Reset the model"""
+        pass
